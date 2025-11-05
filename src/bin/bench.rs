50e885b9f4e70dda062be33ef6877eae13d10091
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-11-05 23:43:52 +0100

    refactor bench

--- src/bin/bench.rs
+++ src/bin/bench.rs
@@ -12,42 +12,33 @@ fn bench_dna_rank<const STRIDE: usize>()
 where
     [(); STRIDE / 4]:,
 {
+    eprint!("DnaRank<{STRIDE:>4}>:");
     let n = 1_000_000_000;
     let q = 1_000_000;
     let seq = b"ACGT".repeat(n / 4);
     let rank = DnaRank::<STRIDE>::new(&seq);
 
     let bits = (rank.mem_size(Default::default()) * 8) as f64 / n as f64;
+    eprint!("{bits:>6.2}b |");
 
     let queries = (0..q)
         .map(|_| rand::random_range(0..seq.len()))
         .collect::<Vec<_>>();
 
-    let start = std::time::Instant::now();
-    for &q in &queries {
-        check(q, rank.ranks_naive(q));
+    fn time(queries: &[usize], f: impl Fn(usize) -> Ranks) {
+        let start = std::time::Instant::now();
+        for &q in queries {
+            check(q, f(q));
+        }
+        let ns = start.elapsed().as_nanos() as f64 / queries.len() as f64;
+        eprint!(" {ns:>5.1}",);
     }
-    let ns_naive = start.elapsed().as_nanos() as f64 / q as f64;
 
-    let start = std::time::Instant::now();
-    for &q in &queries {
-        check(q, rank.ranks_u64(q));
-    }
-    let ns_u64 = start.elapsed().as_nanos() as f64 / q as f64;
-
-    let start = std::time::Instant::now();
-    for &q in &queries {
-        check(q, rank.ranks_u64_prefetch(q));
-    }
-    let ns_u64_pf = start.elapsed().as_nanos() as f64 / q as f64;
-
-    let start = std::time::Instant::now();
-    for &q in &queries {
-        check(q, rank.ranks_u128(q));
-    }
-    let ns_u128 = start.elapsed().as_nanos() as f64 / q as f64;
-
-    eprintln!("DnaRank<{STRIDE:>4}>:  {bits:>6.2} bit  {ns_naive:>5.1} ns  {ns_u64:>5.1} ns  {ns_u64_pf:>5.1} ns  {ns_u128:>5.1} ns",);
+    time(&queries, |p| rank.ranks_naive(p));
+    time(&queries, |p| rank.ranks_u64(p));
+    time(&queries, |p| rank.ranks_u64_prefetch(p));
+    time(&queries, |p| rank.ranks_u128(p));
+    eprintln!();
 }
 
 fn main() {
